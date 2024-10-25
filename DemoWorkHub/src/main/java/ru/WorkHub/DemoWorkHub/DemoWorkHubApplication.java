a75fb976641839a463fe17b7eefca852b55fd51e
Иван <150525201+IvanDvriz-130916y-10@users.noreply.github.com>
2024-10-25 11:56:08 +0500

    Add files via upload

---
+++ DemoWorkHub/src/main/java/ru/WorkHub/DemoWorkHub/DemoWorkHubApplication.java
@@ -0,0 +1,13 @@
+package ru.WorkHub.DemoWorkHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWorkHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWorkHubApplication.class, args);
+	}
+
+}
