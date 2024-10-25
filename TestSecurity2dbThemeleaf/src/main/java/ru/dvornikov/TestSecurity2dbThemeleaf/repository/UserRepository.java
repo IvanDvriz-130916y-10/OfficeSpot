89668e1af558626ba754d587df1a7d80a00f7bbc
Иван <150525201+IvanDvriz-130916y-10@users.noreply.github.com>
2024-10-25 10:33:33 +0500

    Delete TestSecurity2dbThemeleaf directory

--- TestSecurity2dbThemeleaf/src/main/java/ru/dvornikov/TestSecurity2dbThemeleaf/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.dvornikov.TestSecurity2dbThemeleaf.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.dvornikov.TestSecurity2dbThemeleaf.entity.User;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-
-    User findByEmail (String email);
-}
