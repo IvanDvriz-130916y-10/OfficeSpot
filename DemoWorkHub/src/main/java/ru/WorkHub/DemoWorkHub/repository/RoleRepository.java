3889d95330e60026321a64fcb7e96ec077c79f98
Иван <150525201+IvanDvriz-130916y-10@users.noreply.github.com>
2024-10-25 11:54:46 +0500

    Delete DemoWorkHub directory

--- DemoWorkHub/src/main/java/ru/WorkHub/DemoWorkHub/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.WorkHub.DemoWorkHub.repository; // Изменено название пакета на соответствующее проекту WorkHub
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.WorkHub.DemoWorkHub.entity.Role; // Изменено с TestSecurity2dbThemeleaf на WorkHub.DemoWorkHub
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    Role findByName(String name);
-}
